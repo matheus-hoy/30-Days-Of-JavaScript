3df603e1166690270c9809a9c2c0c6163bfda133
matheus <matheus@startaideia.com.br>
2021-06-22 11:06:16 -0300

    resolution day 2. 1-2

---
+++ exercises/day_02_resolution.js
@@ -0,0 +1,5 @@
+// 1- Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+const challenge = '30 Days Of JavaScript';
+
+// 2- Print the string on the browser console using console.log()
+console.log(challenge);
