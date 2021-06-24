0258a670024201755dbb7e2a2f1d3060d519dbab
matheus <matheus@startaideia.com.br>
2021-06-24 10:32:18 -0300

    day2_level2_1to9

---
+++ 02_Day_Data_types/exercises_day2_resolved/day_02_resolution_level2.js
@@ -0,0 +1,66 @@
+// 1 - Using console.log() print out the following statement:
+// The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
+const paragraf1 =
+  "There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.";
+console.log(paragraf1);
+
+// 2 - Using console.log() print out the following quote by Mother Teresa:
+// "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+const paragraf2 =
+  "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.";
+console.log(paragraf2);
+
+// 3 - Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+let number = 10;
+if (typeof '10' == typeof number) {
+  console.log('igual');
+} else {
+  console.log('diferente,', typeof number.toString());
+}
+
+// 4 - Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+let number2 = '9.8';
+
+if (parseFloat(number2) !== 10) {
+  number2 = 10;
+  console.log(number2);
+}
+
+// 5 - Check if 'on' is found in both python and jargon
+let words = 'python jargon';
+words.includes('on');
+if (words.includes('on')) {
+  console.log('TEM');
+} else {
+  console.log('N TEM');
+}
+
+// 6 - I hope this course is not full of jargon. Check if jargon is in the sentence.
+let sentence = 'I hope this course is not full of jargon';
+sentence.includes('jargon');
+if (sentence.includes('jargon')) {
+  console.log('TEM jargon');
+} else {
+  console.log('N TEM jargon');
+}
+
+// 7 - Generate a random number between 0 and 100 inclusively.
+console.log(Math.floor(Math.random() * 101));
+
+// 8 - Generate a random number between 50 and 100 inclusively.
+console.log(Math.floor(Math.random() * (100 - 50)) + 50);
+
+// 9 - Generate a random number between 0 and 255 inclusively.
+console.log(Math.floor(Math.random() * (255 - 0)) + 0);
+
+// 10 - Access the 'JavaScript' string characters using a random number.
+
+
+// 11 - Use console.log() and escape characters to print the following pattern.
+// 1 1 1 1 1
+// 2 1 2 4 8
+// 3 1 3 9 27
+// 4 1 4 16 64
+// 5 1 5 25 125
+
+// 12 - Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
