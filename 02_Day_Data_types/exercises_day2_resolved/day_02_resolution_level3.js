7c9d92eef0de0ea0cff1719190a280320e195f79
matheus <matheus@startaideia.com.br>
2021-06-25 10:44:04 -0300

    day2_level3_complete

--- 02_Day_Data_types/exercises_day2_resolved/day_02_resolution_level3.js
+++ 02_Day_Data_types/exercises_day2_resolved/day_02_resolution_level3.js
@@ -11,60 +11,38 @@ let timesBecause = because.match(/because/gi);
 console.log(timesBecause.length, 'Vezes na frase do because');
 
 // 3 - Clean the following text and find the most frequent word (hint, use replace and regular expressions).
-var sentenceClean =
+const sentence =
   '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
-var remove = /[%$@&#]/gi;
-sentenceClean = sentenceClean.replace(remove, '');
-console.log(sentenceClean);
-//not finish:
-// let mostFrequent = [
-//   'I',
-//   'am',
-//   'a',
-//   'teacher',
-//   'and',
-//   'I',
-//   'love',
-//   'teaching',
-//   'There',
-//   'is',
-//   'nothing',
-//   'as',
-//   'more',
-//   'rewarding',
-//   'as',
-//   'educating',
-//   'and',
-//   'empowering',
-//   'people',
-//   'I',
-//   'found',
-//   'teaching',
-//   'more',
-//   'interesting',
-//   'than',
-//   'any',
-//   'other',
-//   'jobs',
-//   'Does',
-//   'this',
-//   'motivate',
-//   'you',
-//   'to',
-//   'be',
-//   'a',
-//   'teacher',
-//   'This',
-//   '30DaysOfJavaScript',
-//   'is',
-//   'also',
-//   'the',
-//   'result',
-//   'of',
-//   'love',
-//   'of',
-//   'teaching',
-// ];
+
+const forbbidenChars = /[%@&$#;']/gi;
+const clearText = sentence.replace(forbbidenChars, '');
+
+const wordCount = {};
+const words = clearText.split(' ');
+
+let mostFrequentWord = null;
+let mostFrequentNumber = 0;
+
+for (let word of words) {
+  // verifica se a palavra ja foi contada antes
+  if (wordCount[word]) {
+    // se foi, acrecestar +1 na contagem
+    wordCount[word] = wordCount[word] + 1;
+  } else {
+    // se não foi, iniciar a contagem com 1
+    wordCount[word] = 1;
+  }
+
+  // verificar se a franquency da palavra atual é maior do que a ultima palavra analisada
+  if (wordCount[word] > mostFrequentNumber) {
+    // se for, definir a palavra mais franquente como a palavra atual
+    mostFrequentWord = word;
+
+    // atualizar o contador de franquecia para a palavra atual
+    mostFrequentNumber = wordCount[word];
+  }
+}
+console.log(mostFrequentWord);
 
 // 4 - Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
 let salarioMes = 5000;
