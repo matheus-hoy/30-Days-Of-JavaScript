050d81d58f2bd807aedeaff7c55b1d713856b52b
matheus <matheus@startaideia.com.br>
2021-06-28 15:56:28 -0300

    commit_exercises_need_prompt_to_main.js

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,1 +1,83 @@
 // this is your main.js script
+
+// 1 - Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+// const base = prompt('insira a base: ');
+// const altura = prompt('insira a altura:');
+// const area = 0.5 * base * altura;
+// console.log('A área do triangulo é: ', area);
+
+// 2 - Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
+// const ladoA = prompt('Insira lado A: ');
+// const ladoB = prompt('Insira lado B: ');
+// const ladoC = prompt('Insira lado C: ');
+// const perimetro = ladoA + ladoB + ladoC;
+// console.log('Perímetro do triângulo: ', perimetro);
+
+// 3 - Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+// const comprimento = prompt('Insira o comprimento: '); //20
+// const largura = prompt('Insira a largura: '); //10
+// const areaDoRetangulo= (comprimento * largura)
+// const perimetroRetangulo = (2 * (comprimento + largura))
+
+// console.log('A área do retângulo é: ', areaDoRetangulo)
+// console.log('O perímetro do retângulo é: ', perimetroRetangulo)
+
+// 4 - Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
+// const raio = prompt('Insira o raio: '); //10
+// const pi = 3.14;
+// const areaDoCirculo = pi * raio * raio;
+// const circumfDoCirculo = 2 * pi * raio;
+
+// console.log('Área do circulo:', areaDoCirculo);
+// console.log('Circunferencia do circulo: ', circumfDoCirculo);
+
+// 9 - Writ a script that prompt a user to enter hours and rate per hour. Calculate pay of the person?
+// const horasSemanais = prompt('Insira as horas semanais: ');
+// const valorHora = prompt('Insira o valor hora: ');
+// let ganhoSemanal = horasSemanais * valorHora;
+
+// console.log('Seu ganho semanal é de: ', ganhoSemanal);
+
+// 10 - If the length of your name is greater than 7 say, your name is long else say your name is short.
+// let insertName = prompt('Insira seu nome: ');
+// if (insertName.length > 7) {
+//   console.log('Seu nome é LONGO!');
+// } else {
+//   console.log('Seu nome é CURTO!');
+// }
+
+// 12 - Declare two variables myAge and yourAge and assign them initial values and myAge and yourAge.
+// let minhaIdade = prompt('Minha idade = ');
+// let tuaIdade = prompt('Sua idade = ');
+
+// console.log(`Sua idade É ${minhaIdade - yourAge} maior que a minha !`);
+
+// 13 - Using prompt get the year the user was born and if the user is 18 or above allow the user to drive if not tell the user to wait a certain amount of years.
+
+// let birtYear = 2005;
+// year = 2021 - birtYear;
+
+// if (year >= 18) {
+//   console.log('Sua idade é: ', year, 'e você pode dirigir!');
+// } else {
+//   console.log(
+//     'Sua idade é: ',
+//     year,
+//     'e você NÃO pode dirigir, volte daqui a: ',
+//     Math.abs(year - 18.0),
+//     'anos'
+//   );
+// }
+
+// 14 - Write a script that prompt the user to enter number of years. Calculate the number of seconds a person can live. Assume some one lives just hundred years
+// const idade = prompt('digite sua idade: ');
+// const anoNascimento = 2021 - idade;
+// const bornAno = `"${anoNascimento}"`;
+// const nascimento = new Date(bornAno);
+// const now = new Date();
+
+// if (idade <= 100) {
+//   const resultado = now.getTime() - nascimento.getTime();
+//   const totalSegundos = resultado / 1000;
+//   console.log(`Você viveu${totalSegundos} segundos`);
+// }
